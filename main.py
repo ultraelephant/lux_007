35770a87ce4b882f2367b3fbeaaf9fa5b6e4751d
ultraelephant <aleksey@kurnosov.site>
2021-04-23 10:49:37 +0300

    add logging

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import argparse
 
 import config
 import filesvc
+import logging
 import utils
 
 
 def main():
     """Main handler."""
+    logging.basicConfig(level=logging.DEBUG)
     root = config.basepath
     pars = argparse.ArgumentParser()
     pars.add_argument('nargs', nargs='*', default=None, help=argparse.SUPPRESS)
@@ -20,30 +22,30 @@ def main():
             try:
                 args['nargs'][1] = int(args['nargs'][1])
             except Exception as e:
-                print('File name lengh not integer:\n{}'.format(e))
+                logging.error('File name lengh not integer:\n{}'.format(e))
             else:
                 try:
                     filesvc.createfile(root + utils.genname(args['nargs'][1]))
                 except Exception as e:
-                    print(e)
+                    logging.error(e)
 
         elif args['nargs'][0] == 'delete':
             try:
                 filesvc.deletefile(root + args['nargs'][1])
             except Exception as e:
-                print(e)
+                logging.error(e)
 
         elif args['nargs'][0] == 'read':
             try:
                 print(filesvc.readfile(root + args['nargs'][1]))
             except Exception as e:
-                print(e)
+                logging.error(e)
 
         elif args['nargs'][0] == 'getmeta':
             try:
                 print(filesvc.getfilemeta(root + args['nargs'][1]))
             except Exception as e:
-                print(e)
+                logging.error(e)
 
         else:
             print("unknow command")
