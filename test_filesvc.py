b9d996320257f011c7056512cb605e17ba43dcb5
ultraelephant <aleksey@kurnosov.site>
2021-04-23 11:50:55 +0300

    compliance for tests

--- test_filesvc.py
+++ test_filesvc.py
@@ -10,22 +10,27 @@ from filesvc import getfilemeta
 filename = 'testfile'
 filenamepath = basepath + filename
 
+
 def test_createfile():
     createfile(filenamepath)
     with open(filenamepath, 'r') as fp:
         assert fp.read() == ''
 
+
 def test_readfile():
     assert readfile(filenamepath) == ''
 
+
 def test_getfilemeta():
-    attrs = ['st_mode', 'st_ino', 'st_dev', 'st_nlink', 'st_uid', 'st_gid', 'st_size', 'st_atime', 'st_mtime', 'st_ctime']
+    attrs = ['st_mode', 'st_ino', 'st_dev', 'st_nlink', 'st_uid',
+             'st_gid', 'st_size', 'st_atime', 'st_mtime', 'st_ctime']
     for attr in attrs:
         if attr in dir(getfilemeta(filenamepath)):
             pass
         else:
             pytest.fail('Missing {}'.format(attr))
 
+
 def test_deletefile():
     deletefile(filenamepath)
-    assert os.path.isfile(filenamepath) == False
+    assert os.path.isfile(filenamepath) is False
--- test_utils.py
+++ test_utils.py
@@ -5,4 +5,3 @@ from utils import genname
 def test_genname():
     assert len(genname(0)) == 0
     assert len(genname(10)) == 10 and type(genname(10)) is str
-
